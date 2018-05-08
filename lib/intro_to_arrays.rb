ed70364beb88e682a6306e21729220922351c791
Ahmadali <ahmadali.azizov@outlook.com>
2018-05-08 19:19:31 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -1,19 +1,19 @@
 def instantiate_new_array
-  return array = []
+  array = []
 end
 
 def array_with_two_elements
-  return array = [1, 2]
+  array = [1, 2]
 end
 
 def my_first_element(array)
-  return array[0]
+  array[0]
 end
 
 def my_last_element(array)
-  return array[-1]
+  array[-1]
 end
 
 def third_element(array)
-  return array[2]
+  array[2]
 end
